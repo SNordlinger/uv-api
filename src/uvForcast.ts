5f63a6d82da85c2d641f32aaef7a89e9eaedd0ff
Sam Nordlinger <19315410+SNordlinger@users.noreply.github.com>
2021-07-11 18:30:59 -0700

    Make things time zone aware

--- src/uvForcast.ts
+++ src/uvForcast.ts
@@ -1,13 +1,27 @@
-import { LocalDateTime, DateTimeFormatterBuilder } from "@js-joda/core";
+import { LocalDateTime, DateTimeFormatterBuilder, ZoneId } from "@js-joda/core";
 import { Locale } from "@js-joda/locale_en-us";
+import '@js-joda/timezone';
 import axios from "axios";
+import zips from "../data/zips.json";
+import tzlookup from "tz-lookup";
 
 interface UVForcastHour {
-  datetime: LocalDateTime;
+  datetime: string;
   uv: number;
 }
 
-function parseHourlyData(data: any): UVForcastHour[] {
+function timeZoneFromZip(zip: string): ZoneId | null {
+  try {
+    const location = zips[zip as keyof typeof zips];
+    const timeZone = tzlookup(location.latitude, location.longitude);
+    return ZoneId.of(timeZone);
+  } catch (e) {
+    console.log(e)
+    return null
+  }
+}
+
+function parseHourlyData(data: any, timeZone: ZoneId | null): UVForcastHour[] {
   if (!Array.isArray(data)) {
     throw new Error("EPA data is not in array form");
   }
@@ -20,20 +34,24 @@ function parseHourlyData(data: any): UVForcastHour[] {
     }
 
     return {
-      datetime: epaToLocalDateTime(epaDatetime),
+      datetime: epaToIsoTimestamp(epaDatetime, timeZone),
       uv,
     };
   });
 }
 
-function epaToLocalDateTime(epaTimestamp: string) {
+function epaToIsoTimestamp(epaTimestamp: string, timeZone: ZoneId | null): string {
   const formatter = new DateTimeFormatterBuilder()
     .parseCaseInsensitive()
     .appendPattern("MMM/dd/yyyy hh a")
     .toFormatter()
     .withLocale(Locale.US);
-  const datetime = LocalDateTime.parse(epaTimestamp, formatter);
-  return datetime
+  const localDateTime = LocalDateTime.parse(epaTimestamp, formatter);
+  if (timeZone !== null) {
+    const zonedDateTime = localDateTime.atZone(timeZone);
+    return zonedDateTime.toOffsetDateTime().toString();
+  }
+  return localDateTime.toString();
 }
 
 export async function getUVForcastByZipcode(
@@ -42,7 +60,8 @@ export async function getUVForcastByZipcode(
   const endpoint = getZipcodeEndpoint(zip);
   const hourlyResp = await axios.get(endpoint);
   const hourlyEPAData = await hourlyResp.data;
-  return parseHourlyData(hourlyEPAData);
+  const timeZone = timeZoneFromZip(zip);
+  return parseHourlyData(hourlyEPAData, timeZone);
 }
 
 function getZipcodeEndpoint(zipcode: string) {
